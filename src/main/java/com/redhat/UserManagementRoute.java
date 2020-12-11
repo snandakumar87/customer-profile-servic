0aaa50562e316d7314b2b54dfdb2a978f55bd8fa
snandakumar87 <snandaku@redhat.com>
2020-12-10 23:38:29 -0500

    residency service

---
+++ src/main/java/com/redhat/UserManagementRoute.java
@@ -0,0 +1,16 @@
+package com.redhat;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserManagementRoute extends RouteBuilder {
+    @Override
+    public void configure() {
+
+      //TODO add camel route for exposing REST service
+
+    }
+}
